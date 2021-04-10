83b421b8584da245e812032406dfeb912c6ad8ce
MatheusCampos-450 <matheus.campos14@fatec.sp.gov.br>
2021-04-10 12:42:45 -0300

    implementing main page

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainContainer from './styles';
+
+const Main = () => {
+  return (
+    <MainContainer>
+      <h1>teste</h1>
+    </MainContainer>
+  );
+};
+
+export default Main;
