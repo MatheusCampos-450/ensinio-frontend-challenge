d5856db9bbafd4783a1b4f5819042571f14cfbef
MatheusCampos-450 <matheus.campos14@fatec.sp.gov.br>
2021-04-15 01:17:06 -0300

    updating global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,51 +12,39 @@ const GlobalStyle = createGlobalStyle`
     --background: #E5E5E5;
     --black: #130C25;
     --soft-black: #423D51;
+    --soft-gray: #5A5566;
     --water-green: #00E1E7;
     --purple: #432E98;
     --soft-purple: #5F41D9
-
-  }
-
-  @media(max-width: 1980px) {
-    html {
-      /* font-size: 81.25%; */
-    }
   }
 
   @media(max-width: 1680px) {
     html {
-      /* font-size: 78.125%; */
+      font-size: 94.75%;
     }
   }
 
   @media(max-width: 1440px) {
     html {
-      /* font-size: 75%; */
+      font-size: 87.5%;
     }
   }
 
   @media(max-width: 1360px) {
     html {
-      /* font-size: 68.75%; */
+      font-size: 81.25%;
     }
   }
 
   @media(max-width: 1280px) {
     html {
-      /* font-size: 62.5%; */
+      font-size: 78%;
     }
   }
 
-  @media(max-width: 1024px) {
+  @media(max-width: 400px) {
     html {
-      /* font-size: 56.25%; */
-    }
-  }
-
-  @media(max-width: 960px) {
-    html {
-      /* font-size: 50%; */
+      font-size: 72%;
     }
   }
 
